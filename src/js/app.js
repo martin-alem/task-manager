ee304f1cf0bf9e56eec58ebb330ceff47d1e1bfe
Martin Alemajoh <alemajohmartin@gmail.com>
2021-09-15 14:34:14 -0400

    Currency Converter feature complete

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,14 @@
 import weatherController from "./controller/WeatherController.js";
 import newsController from "./controller/NewsController.js";
 import bibleController from "./controller/BibleController.js";
+import currencyController from "./controller/CurrencyController.js";
 
 window.onload = function() {
 
     weatherController.init();
     // newsController.init();
-    bibleController.init();
+    // bibleController.init();
+    currencyController.init();
 
     //calendar functionality
     (function(){

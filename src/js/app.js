3110e56b3a5e0a4536f57682366e28c37a9474e1
Martin Alemajoh <alemajohmartin@gmail.com>
2021-09-15 11:11:37 -0400

    completely implemented the Bible Feature

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 import weatherController from "./controller/WeatherController.js";
 import newsController from "./controller/NewsController.js";
+import bibleController from "./controller/BibleController.js";
 
 window.onload = function() {
 
     weatherController.init();
     // newsController.init();
+    bibleController.init();
 
     //calendar functionality
     (function(){
